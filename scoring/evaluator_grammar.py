3316a606bf05aba85bbf2ea66f6e5361d69872cb
lvap <19362489+lvap@users.noreply.github.com>
2021-02-05 13:25:34 +0100

    further grammar eval error exceptions

--- scoring/evaluator_grammar.py
+++ scoring/evaluator_grammar.py
@@ -14,33 +14,35 @@ def evaluate_grammar(data: WebsiteData) -> float:
     tool = ltp.LanguageTool("en-US")
 
     matches = tool.check(data.headline)
+    matches_to_ignore = 0
+    # ignore error for missing punctuation at title ending
+    if matches[len(matches)-1].ruleId == "PUNCTUATION_PARAGRAPH_END":
+        matches_to_ignore += 1
+        matches.pop()
     matches += tool.check(data.text)
 
+    # filter out irrelevant matches and penalise unknown words as possible errors only once (might be names)
     unknown_words = []
-
-    # filter out irrelevant errors
-    errors_to_ignore = 0
     for match in matches:
-        if (match.ruleId in ["EN_QUOTES", "DASH_RULE"]      # ignore use of improper quote or dash punctuation
-                or match.category == "REDUNDANCY"           # ignore redundancy style warnings
-                or "is British English" in match.message):  # ignore errors for British spelling
-            errors_to_ignore += 1
-        elif match.ruleIssueType == "misspelling":
-            # only penalise unknown words as possible errors once (might be names)
+        if (match.ruleId in ["EN_QUOTES", "DASH_RULE", "EXTREME_ADJECTIVES"]
+                or match.category == "REDUNDANCY"
+                or "is British English" in match.message):
+            matches_to_ignore += 1
+        elif " " not in match.matchedText:
             if match.matchedText in unknown_words:
-                errors_to_ignore += 1
+                matches_to_ignore += 1
             else:
                 unknown_words.append(match.matchedText)
                 print(match)
         else:
             print(match)
 
-    error_score = len(matches) - errors_to_ignore
+    # final error score is 1 - (average errors per word), minimum 0
+    error_score = len(matches) - matches_to_ignore
     word_count = len(data.headline.split()) + len(data.text.split())
-    # error score is 1 - (average errors per word)
     error_score = 1.0 - (error_score / word_count)
 
     print("*** grammar eval: {} errors ({} ignored) in {} words"
-          .format(len(matches) - errors_to_ignore, errors_to_ignore, word_count))
+          .format(len(matches) - matches_to_ignore, matches_to_ignore, word_count))
 
     return error_score if error_score >= 0.0 else 0.0
