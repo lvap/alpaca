0b83deeec313c51dba8a33177edd79ed8ec8cfa0
lvap <19362489+lvap@users.noreply.github.com>
2021-02-07 00:09:36 +0100

    slight changes to readability eval, tokenisation

--- logger.py
+++ logger.py
@@ -3,9 +3,5 @@ DEBUG = True
 
 
 def log(s):
-    """Used to print debugging statements if DEBUG in main is set to True.
-
-    :param s: Statement to be printed to standard output.
-    """
     if DEBUG:
         print(s)
--- scoring/evaluator_readability.py
+++ scoring/evaluator_readability.py
@@ -1,5 +1,4 @@
 from nltk.tokenize import sent_tokenize
-import readability
 
 from logger import log
 from parsing.website_data import WebsiteData
@@ -15,9 +14,8 @@ def evaluate_readability(data: WebsiteData) -> float:
 
     headline_period = "" if has_ending_punctuation(data.headline) else ". "
     full_text = (data.headline + headline_period + data.text).replace("\n", " ")
+    # TODO poor tokenization for quotation marks at end of sentences. use syntok?
     tokens = sent_tokenize(full_text)
     log("*** Tokenisation: {} sentences".format(len(tokens)))
 
-    # readability_results = readability.getmeasures(tokens, lang="en")
-    # log(readability_results)
     return 1.0
